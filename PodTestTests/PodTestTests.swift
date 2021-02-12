194b9cdf3a9a5e7e2cc6340983136bac440889c5
Somnath Chatterjee <somnath.chatterjee121@gmail.com>
2021-02-12 17:03:27 +0700

    Added Test Cases

--- PodTestTests/PodTestTests.swift
+++ PodTestTests/PodTestTests.swift
@@ -10,24 +10,18 @@ import XCTest
 
 class PodTestTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var podTest: PodTest!
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        override func setUp() {
+            podTest = PodTest()
         }
-    }
+
+        func testAdd() {
+            XCTAssertEqual(podTest.add(a: 1, b: 1), 2)
+        }
+        
+        func testSub() {
+            XCTAssertEqual(podTest.sub(a: 2, b: 1), 1)
+        }
 
 }
