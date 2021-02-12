c5db740ed9fc0108f9ed9b68358033d03a88ff92
Somnath Chatterjee <somnath.chatterjee121@gmail.com>
2021-02-13 01:14:02 +0700

    Update PodTestTests.swift

--- PodTestTests/PodTestTests.swift
+++ PodTestTests/PodTestTests.swift
@@ -17,7 +17,7 @@ class PodTestTests: XCTestCase {
         }
 
         func testAdd() {
-            XCTAssertEqual(podTest.add(a: 1, b: 1), 2)
+            XCTAssertEqual(podTest.add(a: 3, b: 1), 4)
         }
         
         func testSub() {
