6f9d3c0f2d88b93d5b6cec88ab05e3d35b644747
Somnath Chatterjee <somnath.chatterjee121@gmail.com>
2021-02-12 17:03:27 +0700

    Added Test Cases

---
+++ PodTest/PodTest.swift
@@ -0,0 +1,22 @@
+//
+//  PodTest.swift
+//  PodTest
+//
+//  Created by Somnath Chatterjee on 2/12/21.
+//
+
+import Foundation
+
+public final class PodTest {
+
+    let name = "PodTest"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
